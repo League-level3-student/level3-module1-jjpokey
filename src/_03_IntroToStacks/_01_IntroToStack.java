36afc81cedeaec678924afc5058e74a61ff85a38
League Student <league@iMac-7.attlocal.net>
2019-07-12 16:27:29 -0700

    Stacks

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -7,16 +7,36 @@ import javax.swing.JOptionPane;
 
 public class _01_IntroToStack {
 	public static void main(String[] args) {
+		int first = 0;
+		int second = 0;
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> s = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		for(int i = 0; i < 101; i++) {
+			Random r = new Random();
+			s.push(r.nextDouble() * 100);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		
+		String number = JOptionPane.showInputDialog("Enter A Number Between 0 and 99");
+		String number2 = JOptionPane.showInputDialog("Enter A Greater Number Between 1 and 100");
+		
+		int p = Integer.parseInt(number);
+		int q = Integer.parseInt(number2);
 		
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
+		for(int i = 0; i < s.size(); i++){
+			Double v = s.pop();
 		
+			if(v < q && v > p) {
+				System.out.println(v);
+			}
+		
+		
+		
+		}
 		
 		//   EXAMPLE:
 		//   NUM 1: 65
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,18 +1,50 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
 	/* 
 	 * Create a JFrame with a JPanel and a JLabel.
-	 * 
+	 * DONE
 	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
-	 * 
+	 * DONE
 	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
 	 * Save that deleted character onto a Stack of Characters.
-	 * 
+	 * WORKING ON
 	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
 	 * off the Stack and added back to the JLabel.
 	 * 
 	 * */
-	
-	
+	  JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+
+	public void method() {
+		frame.add(panel);
+		frame.add(label);
+		frame.addKeyListener(this);
+	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		if(e.getKeyChar() == KeyEvent.VK_BACK_SPACE) {
+			//label.remove();
+			//HERE, REMOVE LAST CHAR TYPED............................
+		}
+		
+	}
+	@Override
+	public void keyPressed(KeyEvent e) {
+		label.setText(e.getKeyChar() + "");
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }

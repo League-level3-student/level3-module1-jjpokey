4a9020294a7dde60baa60f78fe16b582ffe38f63
League Student <league@iMac-15.attlocal.net>
2019-09-16 16:59:40 -0700

    hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -2,6 +2,8 @@ package _04_HangMan;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
+import java.util.Stack;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -10,33 +12,61 @@ import javax.swing.JPanel;
 
 public class HangMan implements KeyListener{
 	
-	public static void main(String[] args) {		
-		//beginning dialog
-		//String number = JOptionPane.showInputDialog("Enter a number 2 through 12");
-		//int num = Integer.parseInt(number);
-		HangMan h = new HangMan();
-		h.method();
-		
-		
-		
-		
-	}
-	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
+	static int charNumber = 0;
+	String charsinword = "";
+	int Lives = 10;
+	static String characters = "";
+	
+	public static void main(String[] args) {
+		//method calling and starting input
+		Stack<String> stack = new Stack<String>();
+		String number = JOptionPane.showInputDialog("Welcome to Hang Man! \n" + "How many words would you like to guess?");
+		int num = Integer.parseInt(number);
+		HangMan h = new HangMan();
+		h.method();
+		
+		//getting random words/getting char number of words
+		for(int i = 0; i < num; i++) {
+			stack.push(Utilities.readRandomLineFromFile("dictionary.txt"));
+	
+	}
+		for(int i = 0; i < num; i++) {
+			characters = stack.pop();
+			
+			charNumber = characters.length();
+			
+	
+	}
+		h.getChars();
+	} //end of main
+	
+	
 	
 	public void method() {
+		//frame setup
 		frame.add(panel);
 		panel.add(label);
-		frame.setName("Hang Man");
-		frame.addKeyListener(this);
+		frame.setTitle("Hang Man");
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		frame.pack();
-		frame.setSize(800, 500);
+		frame.setSize(200, 50);
+		label.addKeyListener(this);
 		
 	}
+	public void getChars() {
+		for(int a = 0; a < charNumber; a++) {
+			charsinword += "_";
+					
+			label.setText(charsinword + "     " + Lives + " lives left!");
+		}
+		
+	}
+
+
 
 	@Override
 	public void keyTyped(KeyEvent e) {
@@ -44,17 +74,36 @@ public class HangMan implements KeyListener{
 		
 	}
 
+
+
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		String t = "";
+		for(int j = 0; j < charNumber; j++) {
+			
+		if(e.getKeyChar() == characters.charAt(j)) {
+			t += e.getKeyChar();
+			//charsinword.replace(charsinword.charAt(j), e.getKeyChar());
+			
+		}
+		else {
+			t += charsinword.charAt(j);
+			//figure out loop and replacing
+		}
 		
+		}
+		charsinword = t;
 	}
+
+
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+
+
 		
 	}
 	

3fda4af4fb9bdfc0e6060879df40495e4ed2a0d7
League Student <league@iMac-27.attlocal.net>
2019-10-14 16:57:59 -0700

    hashmaps

--- src/_05_String_Integer_Pair/StringIntegerPair.java
+++ src/_05_String_Integer_Pair/StringIntegerPair.java
@@ -1,12 +1,14 @@
 package _05_String_Integer_Pair;
 
 public class StringIntegerPair {
-	//1. create a private array of Strings called keys. Don't initialize it.
-
+	//1. create a private array of Strings called keys. 
+	private String[] keys;
 	//2. create a private array of integers called values.
-	
+	private int[] values;
 	StringIntegerPair(){
 		//3. initialize both member arrays to a length of 0
+		keys = new String[0];
+		values = new int[0];
 	}
 	
 	// 4. Complete the steps in the put method
@@ -14,29 +16,52 @@ public class StringIntegerPair {
 		//A. iterate through the keys. If you find a key that matches
 		//   the passed in String, set the value at that location to the
 		//   passed in value and return from the method.
-		
+		for(int i = 0; i < keys.length; i++) {
+			if(key.equals(keys[i])) {
+				values[i] = value;
+				return;
+			}
+			
+		}
 		//B. create a String array that is one element longer than the keys
-		
+		String[] moreKeys = new String[keys.length + 1];
 		//C. create an integer array that is one element longer than values
-		
+		int[] moreValues = new int[values.length + 1];
 		//D. set the last element of the new String array to the passed in key
-		
+		moreKeys[moreKeys.length - 1] = key;
 		//E. set the last element of the new int array to the passed in value
-		
+		moreValues[moreValues.length - 1] = value;
 		//F. iterate through the keys and values and copy the elements to the new arrays
-		
+		for(int a = 0; a < keys.length; a++) {
+			moreKeys[a] = keys[a];
+		}
+		for(int e = 0; e < values.length; e++) {
+			moreValues[e] = values[e];
+		}
 		//G. Set the keys and values arrays equal to the new arrays created in steps B and C.
+		keys = moreKeys;
+		values = moreValues;
 	}
 	
 	//5. Complete the method so it returns the value located at the passed in key.
 	//   If the key does not exist, return Integer.MIN_VALUE.
 	public int get(String key) {
-		return 0;
+		
+		for(int i = 0; i < keys.length; i++) {
+			if(key.equals(keys[i])) {
+				return values[i];
+			}
 	}
-	
+	return Integer.MIN_VALUE;
+	}
 	//6. Complete the containsKey method so that it returns true if the
 	//   passed in keys is contained in the keys array
 	public boolean containsKey(String key) {
+		for(int d = 0; d < keys.length; d++) {
+			if(keys[d].equals(key)) {
+				return true;
+			}
+		}
 		
 		return false;
 	}
@@ -44,17 +69,21 @@ public class StringIntegerPair {
 	//7. Complete the containsValue method so that it returns true if the
 	//   passed in value is contained in the values array
 	public boolean containsValue(int value) {
-		
+		for(int f = 0; f < values.length; f++) {
+			if(values[f] == value) {
+				return true;
+			}
+		}
 		return false;
 	}
 	
 	//8. Complete the getKeysMethod so it returns the keys as an array
 	public String[] getKeys() {
-		return null;
+		return keys;
 	}
 	
 	//9. Complete the getValues so it returns the values as an array
 	public int[] getValues() {
-		return null;
+		return values;
 	}
 }
